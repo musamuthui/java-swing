bd40737dec4406eb1dbd68ffc2b195765a6056c8
Musa Muthui <154660680+musamuthui@users.noreply.github.com>
2026-05-22 10:21:03 +0000

    Update .java file

---
+++ MyWindow.java
@@ -0,0 +1,7 @@
+JFrame frame = new JFrame("Frame Demo");
+
+frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+frame.pack();
+frame.setVisible(true);
+
